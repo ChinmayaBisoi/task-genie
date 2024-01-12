02d4dddd85fca7070d26ae9754d66dc7cd401d1d
C_bi__ <chinmaya@kloudle.com>
2024-01-13 00:19:21 +0530

    Add : 404 page - ui improvements

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "~/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 
 const PageNotFound = () => {
   return (
@@ -10,11 +11,15 @@ const PageNotFound = () => {
         <Image src="/logo.svg" alt="logo" width={28} height={28} />
         <span className="text-3xl font-semibold text-brand-dark">Mello</span>
       </Link>
+      <Link href="/" className="absolute right-5 top-5">
+        <Button variant="ghost">
+          <ChevronLeft className="mr-2" /> Back to Homepage
+        </Button>
+      </Link>
+
       <h2 className="text-5xl font-semibold">404</h2>
+
       <h1 className="font-bold">Page not found</h1>
-      <Button className="bg-brand-light hover:bg-brand-dark">
-        Back to Homepage
-      </Button>
     </div>
   );
 };

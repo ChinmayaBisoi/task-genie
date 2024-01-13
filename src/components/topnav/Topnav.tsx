ffcb12ac6970f111ca7048a0954a346f4c7c236d
C_bi__ <chinmaya@kloudle.com>
2024-01-13 16:07:58 +0530

    Add : Topnav component

--- src/components/topnav/Topnav.tsx
+++ src/components/topnav/Topnav.tsx
@@ -1,21 +1,31 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import React from "react";
-import { Button } from "../ui/button";
+import Login from "../common/Login";
+import { UserMenuDropdown } from "./UserMenuDropdown";
+import Link from "next/link";
 
-const Topnav = ({ signIn }: { signIn: () => Promise<any> }) => {
+const Topnav = () => {
+  const { data, status } = useSession();
+  const loading = status === "loading";
+  const user = data?.user;
+  const userImg = user?.image;
+  const userName = user?.name;
+  const userExists = user && userImg && userName;
+
+  const showLogin = !loading && !userExists;
+  const showUserMenu = !loading && userExists;
+
   return (
     <div className="sticky top-0 flex w-full bg-white shadow-lg">
-      <div className="flex items-center gap-2 px-4 py-2">
-        <Image src="/logo.svg" alt="logo" width={28} height={28} />
-        <span className="text-brand-dark text-2xl font-bold">TaskGenie</span>
-      </div>
-      <nav className="ml-auto">
-        <Button
-          onClick={signIn}
-          className="bg-brand-light hover:bg-brand-dark h-full rounded-none px-6 text-lg font-semibold"
-        >
-          Login
-        </Button>
+      <Link href={data ? "/dashboard" : "/"} className="mx-4 my-2">
+        <div className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={28} height={28} />
+          <span className="text-2xl font-bold text-brand-dark">TaskGenie</span>
+        </div>
+      </Link>
+      <nav className="ml-auto flex items-center">
+        <Login />
+        <UserMenuDropdown />
       </nav>
     </div>
   );

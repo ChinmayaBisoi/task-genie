99a6099bf978f6a66e4b4f743e42e6e047e33f33
C_bi__ <chinmaya@kloudle.com>
2024-01-14 14:17:31 +0530

    Add : Popup component

---
+++ src/components/common/Popup.tsx
@@ -0,0 +1,34 @@
+import { X } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+const Popup = ({
+  show = false,
+  onClose = () => {},
+  className = "",
+  children,
+}: {
+  show: boolean;
+  onClose: () => void;
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  if (!show) return null;
+  return (
+    <div className="fixed left-0 top-0 z-10 flex h-screen w-screen items-center justify-center bg-neutral-600/70 backdrop-blur-sm">
+      <div
+        className={`relative h-screen w-screen rounded-lg bg-white p-4 shadow-md md:max-h-[80vh] md:max-w-md ${className}`}
+      >
+        <button
+          className="absolute right-4 top-4 rounded-md p-1 hover:bg-neutral-100"
+          onClick={onClose}
+        >
+          <X />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;

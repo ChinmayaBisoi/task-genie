f2edb0dab47d9313cb6eeeb02c5ab44781f71cd9
C_bi__ <chinmaya@kloudle.com>
2024-01-14 15:35:19 +0530

    Feat : Add dynamic Link to topnav logo

--- src/components/topnav/Topnav.tsx
+++ src/components/topnav/Topnav.tsx
@@ -9,11 +9,12 @@ const Topnav = () => {
 
   return (
     <div className="sticky top-0 flex w-full bg-white shadow-lg">
-      <Link href={data ? "/dashboard" : "/"} className="mx-4 my-2">
-        <div className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="logo" width={28} height={28} />
-          <span className="text-2xl font-bold text-brand-dark">TaskGenie</span>
-        </div>
+      <Link
+        href={data ? "/dashboard" : "/"}
+        className="mx-4 my-2 flex cursor-pointer items-center gap-2"
+      >
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <span className="text-2xl font-bold text-brand-dark">TaskGenie</span>
       </Link>
       <nav className="ml-auto flex items-center">
         <Login />

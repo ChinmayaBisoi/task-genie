bdc113dd7aa0151c0fd633c95203e4c9315577e9
C_bi__ <chinmaya@kloudle.com>
2024-01-17 01:56:46 +0530

    Add : Sidebar changes for project details page

--- src/components/common/sidebar/Sidebar.tsx
+++ src/components/common/sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import styles from "./sidebar.module.css";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import Avatar from "~/components/Avatar";
 
 export const NAV_ITEMS = [
   { label: "Dashboard", link: "/dashboard" },
@@ -12,26 +14,35 @@ export const NAV_ITEMS = [
 
 const Sidebar = () => {
   const { pathname } = useRouter();
+  const noActivePaths = !NAV_ITEMS.find((item) => pathname.includes(item.link));
+
   return (
     <aside
-      className={`${styles.customHeight} sticky top-12 hidden md:inline-block md:w-52`}
+      className={`${styles.customHeight} sticky top-12 hidden md:flex md:w-52 md:flex-col md:justify-between`}
     >
-      <ul className="my-8 space-y-2 px-4 text-sm">
-        {NAV_ITEMS.map((item) => {
-          const isActive = pathname.includes(item.link);
-          return (
-            <li key={item.label}>
-              <Link
-                href={item.link}
-                className={`block rounded-full px-4 py-2 font-medium  hover:bg-brand-light hover:text-white 
-                ${isActive ? "bg-brand-light text-white" : "text-gray-700"}`}
-              >
-                {item.label}
-              </Link>
+      <nav>
+        <ul className="my-8 space-y-2 px-4 text-sm">
+          {noActivePaths && (
+            <li className="ml-4 rounded-sm pr-4 pt-1 text-xs font-medium text-gray-700 underline">
+              Quick Links
             </li>
-          );
-        })}
-      </ul>
+          )}
+          {NAV_ITEMS.map((item) => {
+            const isActive = pathname.includes(item.link);
+            return (
+              <li key={item.label}>
+                <Link
+                  href={item.link}
+                  className={`block rounded-full px-4 py-2 font-medium  hover:bg-brand-light hover:text-white 
+                ${isActive ? "bg-brand-light text-white" : "text-gray-700"}`}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </aside>
   );
 };
